9b224827739f1f8399cda9903d233a4b8d92240a
Patrick Castro <patrickcastro117@yahoo.com>
2024-04-25 14:11:28 -0700

    Added Email Sending Functionality

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, BackgroundTasks
+from send_email import send_email_async
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 import models
@@ -36,6 +37,7 @@ class UserModel(UserBase):
     class Config:
         orm_mode = True
 
+
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -43,7 +45,7 @@ def get_db():
         yield db
     finally:
         db.close()
-    
+
 @app.get("/users/", response_model=UserModel)
 def read_users(limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.Users).limit(limit).all()
@@ -56,6 +58,14 @@ def read_user_by_email(email: str, db: Session = Depends(get_db)):
     else:
         return result    
 
+@app.get("/confirm/async/")
+async def confirm_email(db: Session = Depends(get_db)):
+    await send_email_async('Hello World', '{models.Users.email}', {
+        'title': 'Confirm Email', 
+        'name': 'John Doe'
+    })
+    return 'Success'
+
 @app.post("/users/", response_model=UserModel)
 def create_user(user: UserBase, db: Session = Depends(get_db)):
     db_user = models.Users(first_name=user.first_name, last_name=user.last_name, email=user.email, password=user.password)
@@ -65,4 +75,5 @@ def create_user(user: UserBase, db: Session = Depends(get_db)):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+    
     return db_user
---
+++ api/send_email.py
@@ -0,0 +1,41 @@
+import os
+from fastapi import BackgroundTasks
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+class Envs:
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_FROM = os.getenv('MAIL_FROM')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
+    
+conf = ConnectionConfig(
+    MAIL_USERNAME=Envs.MAIL_USERNAME,
+    MAIL_PASSWORD=Envs.MAIL_PASSWORD,
+    MAIL_FROM=Envs.MAIL_FROM,
+    MAIL_PORT=Envs.MAIL_PORT,
+    MAIL_SERVER=Envs.MAIL_SERVER,
+    MAIL_FROM_NAME=Envs.MAIL_FROM_NAME,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER='./templates'
+)
+
+async def send_email_async(subject: str, email_to: str, body: dict):
+    message = MessageSchema(
+        subject=subject,
+        recipients=[email_to],
+        template_body=body,
+        subtype='html',
+    )
+    print(conf.MAIL_USERNAME)
+
+    fm = FastMail(conf)
+    # background_tasks.add_task(
+    await fm.send_message(message, template_name='confirm_email.html')
